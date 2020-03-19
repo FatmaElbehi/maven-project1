182f885d8eb3a411423a2e294f7a2e18011a77c8
unknown <fatmaelbehi9@example.com>
2020-03-19 10:23:18 +0100

    My Second Maven Project

---
+++ src/main/java/com/somme/demo/Somme.java
@@ -0,0 +1,15 @@
+package com.somme.demo;
+
+public class Somme {
+	public static void main(String[] args) {
+
+	int n=4;
+	int s=0;
+	
+	for (int i=0;i<n;i++)
+	{
+		s+=i;
+	}
+	System.out.println ("La somme des entier sont: "+s );
+}
+}
---
+++ src/test/java/com/test/demo/TestDemo.java
@@ -0,0 +1,17 @@
+package com.test.demo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.somme.demo.Somme;
+
+public class TestDemo {
+
+	@Test
+	public void test() {
+		System.out.println("Success");
+		
+	}
+
+}
